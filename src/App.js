8ec68916303476f623a3b8320ce41fb96e346709
Luiz G. Duarte Ramos <luiz.g.duarte.ramos@avanade.com>
2019-09-15 11:55:03 -0300

    Criou o component Header sem o css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';  //, { Fragment } opcional se fosse usar o Fragment no wrapper
-import Sobre from './objects/Sobre';
-import Logo from './objects/Logo';
-import Menu from './objects/Menu';
+import Header from './components/Header';
 
+// Em versões atuais do JSX, não é mais necessário
+// importar o Fragment do react, basta envolver o 
+// <Fragment>
+//     <Logo />
+//     <Sobre />
+// </Fragment>
+// wrapper com <></>
 
 function App() {
     return (
-        // Em versões atuais do JSX, não é mais necessário
-        // importar o Fragment do react, basta envolver o 
-        // <Fragment>
-        //     <Logo />
-        //     <Sobre />
-        // </Fragment>
-        // wrapper com <></>
-        <>
-            <Logo />
-            <Sobre />
-            <Menu />
-        </>
+        <Header />
     );
 }
 
