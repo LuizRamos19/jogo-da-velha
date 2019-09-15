4433c4996788c89bb364375ab49211070d67c77e
Luiz G. Duarte Ramos <luiz.g.duarte.ramos@avanade.com>
2019-09-15 10:46:14 -0300

    Criado o object Logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from 'react';  //, { Fragment } opcional se fosse usar o Fragment no wrapper
 import Sobre from './objects/Sobre';
+import Logo from './objects/Logo';
 
 function App() {
     return (
-        <Sobre />
+        // Em versões atuais do JSX, não é mais necessário
+        // importar o Fragment do react, basta envolver o 
+        // <Fragment>
+        //     <Logo />
+        //     <Sobre />
+        // </Fragment>
+        // wrapper com <></>
+        <>
+            <Logo />
+            <Sobre />
+        </>
     );
 }
 
