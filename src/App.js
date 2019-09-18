9df019c3e554b1a6d7e8826769084ae8ec7db2ea
LuizRamos19 <luizgdramos@gmail.com>
2019-09-18 13:28:37 -0300

    Criando o checkbox com before e retirando o input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';  //, { Fragment } opcional se fosse usar o Fragment no wrapper
 import './App.css';
 
-import Input from './objects/Input';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
 import Checkbox from './objects/Checkbox';
--- src/objects/Checkbox/index.js
+++ src/objects/Checkbox/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './styles.css';
+
 import Input from '../Input';
 import Label from '../Label';
 
 const Checkbox = ({ id = "", value = "", content = "" }) => (
     <>
-        <Input id={id} value={value} type="checkbox" content={content} />
-        <Label htmlFor={id} content={content} />
+        <Input className="input" id={id} value={value} type="checkbox" content={content} />
+        <Label className="label" htmlFor={id} content={content} />
     </>
 );
 
